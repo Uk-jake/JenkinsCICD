ae6d22930322854a3f5cbce59793eb618331930f
UkJang <mag0225@naver.com>
2024-11-11 09:43:20 +0900

    init: SpringBoot

---
+++ src/main/java/com/jake/jenkinscicdproject/JenkinsCicdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jake.jenkinscicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jake/jenkinscicdproject/JenkinsCicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.jenkinscicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
