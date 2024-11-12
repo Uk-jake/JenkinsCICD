2f353c0ada5fbe0efe480cddaef866e3e5dad232
UkJang <mag0225@naver.com>
2024-11-12 17:43:48 +0900

    finish 1112

--- src/main/java/com/jake/jenkinscicdproject/JenkinsServices.java
+++ src/main/java/com/jake/jenkinscicdproject/JenkinsServices.java
@@ -1,9 +1,12 @@
 package com.jake.jenkinscicdproject;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JenkinsServices {
+    // 캐싱 기능
+    //@Cacheable("sum")
     public int sum(int a, int b) {
         return a + b;
     }
